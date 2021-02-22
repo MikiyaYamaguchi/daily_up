1102abf20a50f056242870c97fb567fc8a15c470
MikiyaYamaguchi <mikiyagreeeen@gmail.com>
2021-02-23 00:24:50 +0900

    gitキャッシュクリア

--- wp-content/themes/seeding_theme/index.php
+++
@@ -1,8 +0,0 @@
-<?php get_header(); ?>
-<main>
-  <div class="container">
-    <section>
-    </section>
-  </div>
-</main>
-<?php get_footer(); ?>
--- wp-content/themes/seeding_theme/searchform.php
+++
@@ -1,8 +0,0 @@
-<div id="search">
-	<form role="search" action="<?php bloginfo('url'); ?>" method="get" id="searchform" class="searchform">
-		<div class="search_wrap">
-		<input type="text" value="<?php echo get_search_query(); ?>" name="s" id="s" placeholder="キーワード検索"/>
-			<input type="submit" id="searchsubmit" value="" />
-		</div>
-	</form>
-</div>
