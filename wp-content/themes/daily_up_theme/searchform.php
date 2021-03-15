c5cf89a2b272ffc2fc827f399501343cf5a65b83
MikiyaYamaguchi <mikiyagreeeen@gmail.com>
2021-03-15 23:17:24 +0900

    検索絞り込み機能実装（キーワード、カテゴリー、タグ）

--- wp-content/themes/daily_up_theme/searchform.php
+++ wp-content/themes/daily_up_theme/searchform.php
@@ -2,6 +2,23 @@
 	<form role="search" action="<?php bloginfo('url'); ?>" method="get" id="searchform" class="searchform">
 		<div class="search-wrap">
 			<input type="text" value="<?php echo get_search_query(); ?>" name="s" id="s" placeholder="キーワード検索" />
+			<?php
+			$args = array(
+				'show_option_all'   => 'カテゴリー選択',
+				'echo'               => 1,
+				'selected'           => 0,
+			);
+			wp_dropdown_categories($args);
+			?>
+			<?php $tags = get_tags();
+			if ($tags) : ?>
+				<select name='tag' id='tag'>
+					<option value="" selected="selected">タグ選択</option>
+					<?php foreach ($tags as $tag) : ?>
+						<option value="<?php echo esc_html($tag->slug);  ?>"><?php echo esc_html($tag->name); ?></option>
+					<?php endforeach; ?>
+				</select>
+			<?php endif; ?>
 			<input type="submit" id="searchsubmit" value="検索" />
 		</div>
 	</form>
