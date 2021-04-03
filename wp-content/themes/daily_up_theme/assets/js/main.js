22c9932ec2d918858f65e60665607dd6a99a6a7d
MikiyaYamaguchi <mikiyagreeeen@gmail.com>
2021-04-03 22:15:52 +0900

    サイドメニュー調整、コンテンツ幅調整（後に再調整の必要性あるかも）

---
+++ wp-content/themes/daily_up_theme/assets/js/main.js
@@ -0,0 +1,24 @@
+$(document).ready(function () {
+  $(function () {
+    var target = $(".side-column"); //ここに追尾したい要素名を記載
+    var footer = $("footer"); //フッターでストップさせる
+    var targetHeight = target.outerHeight(true);
+    var targetTop = target.offset().top;
+
+    $(window).scroll(function () {
+      var scrollTop = $(this).scrollTop();
+      if (scrollTop > targetTop) {
+        // 動的にコンテンツが追加されてもいいように、常に計算する
+        var footerTop = footer.offset().top;
+
+        if (scrollTop + targetHeight > footerTop) {
+          target.addClass("fixed_side");
+        } else {
+          target.addClass("fixed_side");
+        }
+      } else {
+        target.removeClass("fixed_side");
+      }
+    });
+  });
+});
