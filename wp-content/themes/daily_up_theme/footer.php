71a19924f72310797ef86ab659f1f49748485854
MikiyaYamaguchi <mikiyagreeeen@gmail.com>
2021-04-05 00:20:36 +0900

    footerコーディング（検索部分はヘッダーのデザインに合わせるため一旦待機）、全体のデザイン調整

--- wp-content/themes/daily_up_theme/footer.php
+++ wp-content/themes/daily_up_theme/footer.php
@@ -1,15 +1,21 @@
 <div class="pagetop"><a href="#wrap"></a></div>
 <footer>
   <div class="container">
-    <?php
-    wp_nav_menu(array(
-      'theme_location' => 'footer',
-      'container'      => 'div',
-      'depth'          => 1,
-      'menu_class'     => 'footer_nav',
-    ));
-    ?>
-    <p class="text-center">Copyright &copy; <?php bloginfo('name'); ?> All Rights Reserved.</p>
+    <div class="ftr-search-wrap">
+      <?php get_search_form(); ?>
+    </div>
+    <div class="ftr-content-wrap">
+      <?php
+      wp_nav_menu(array(
+        'theme_location' => 'footer',
+        'container'      => 'div',
+        'depth'          => 1,
+        'menu_class'     => 'footer_nav',
+        'items_wrap'      => '<ul>%3$s<li class="sns_btn"><a href="#" target="_blank"><img src="/daily_up/wp-content/uploads/twitter_icon_bk.png" alt="Twitter"></a></li></ul>',
+      ));
+      ?>
+      <p class="copy-light">Copyright &copy; <?php bloginfo('name'); ?> All Rights Reserved.</p>
+    </div>
   </div>
 </footer>
 <script type="text/javascript" src="<?php echo get_template_directory_uri(); ?>/assets/js/jquery-3.5.1.min.js"></script>
